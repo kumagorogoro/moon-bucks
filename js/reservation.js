9f3e767e2f4f55c2d9cc8384a45e22a51850fa62
kumagorogoro <marustage.ooo@gmail.com>
2025-01-20 18:58:26 +0900

    meal error

--- js/reservation.js
+++ js/reservation.js
@@ -1,65 +1,9 @@
-// ハンバーガーメニュー
-const hamburger = document.querySelector("#menubtn");
-const menubeans = document.querySelector(".menubeans");
-const menu = document.querySelector("#spmenu");
-const closeBtn = document.getElementById("close");
-const menutext = document.querySelector("#menutext");
-const links = document.querySelectorAll("#spmenu li a");
-menubeans.addEventListener("click", function () {
-  menu.classList.add("show");
-});
-hamburger.addEventListener("click", function () {
-  menu.classList.add("show");
-});
-closeBtn.addEventListener("click", function () {
-  menu.classList.remove("show");
-});
-window.addEventListener("click", function (e) {
-  if (
-    !menu.contains(e.target) &&
-    e.target !== hamburger &&
-    e.target !== menutext &&
-    e.target !== menubeans
-  ) {
-    menu.classList.remove("show");
-  }
-});
-
-// pc-nav
-let lastScrollTop = 0;
-const navbar = document.querySelector("nav");
-
-window.addEventListener("scroll", function () {
-  const currentScroll = window.scrollY || document.documentElement.scrollTop;
-  if (currentScroll > lastScrollTop) {
-    navbar.style.top = "-150px";
-  } else {
-    navbar.style.top = "0";
-  }
-  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-});
-
-const navLinks = document.querySelectorAll("nav ul li a");
-navLinks.forEach((link) => {
-  link.addEventListener("click", function () {
-    if (link.textContent === "Cafe") {
-      return;
-    } else if (link.textContent === "Stay") {
-      return;
-    } else {
-      navbar.style.top = "-150px";
-    }
-  });
-});
-
-window.addEventListener("mousemove", function (event) {
-  const mouseY = event.clientY;
-  if (mouseY < 50) {
-    navbar.style.top = "0";
-  }
-});
-
 // form
+// load
+window.addEventListener("load", function () {
+  toggleMealOptions();
+});
+
 const agreeCheckbox = document.getElementById("agree");
 const submitBtn = document.getElementById("submit-btn");
 agreeCheckbox.addEventListener("change", () => {
@@ -69,9 +13,7 @@ agreeCheckbox.addEventListener("change", () => {
     submitBtn.disabled = true;
   }
 });
-if (!agreeCheckbox.checked) {
-  submitBtn.disabled = true;
-}
+
 function toggleMealOptions() {
   const plan = document.querySelector('input[name="plan"]:checked').value;
   const dinnerCheckbox = document.getElementById("dinner");
@@ -95,11 +37,13 @@ reservationForm.addEventListener("submit", function (event) {
     span.textContent = "";
   });
   const errors = [];
+
   const name = document.getElementById("name").value;
   if (!name) {
     document.getElementById("nameError").textContent = "名前は必須です。";
     errors.push("名前は必須です。");
   }
+
   const postalCode = document.getElementById("postal_code").value;
   if (!postalCode) {
     document.getElementById("postalCodeError").textContent =
@@ -110,23 +54,27 @@ reservationForm.addEventListener("submit", function (event) {
       "郵便番号は正しい形式で入力してください（例: 123-4567）。";
     errors.push("郵便番号は正しい形式で入力してください（例: 123-4567）。");
   }
+
   const prefecture = document.getElementById("prefecture").value;
   if (!prefecture) {
     document.getElementById("prefectureError").textContent =
       "都道府県は必須です。";
     errors.push("都道府県は必須です。");
   }
+
   const city = document.getElementById("city").value;
   if (!city) {
     document.getElementById("cityError").textContent = "市区町村は必須です。";
     errors.push("市区町村は必須です。");
   }
+
   const address = document.getElementById("address").value;
   if (!address) {
     document.getElementById("addressError").textContent =
       "番地・建物名は必須です。";
     errors.push("番地・建物名は必須です。");
   }
+
   const email = document.getElementById("email").value;
   if (!email) {
     document.getElementById("emailError").textContent =
@@ -137,11 +85,34 @@ reservationForm.addEventListener("submit", function (event) {
       "正しい形式でメールアドレスを入力してください。";
     errors.push("正しい形式でメールアドレスを入力してください。");
   }
+
   const phone = document.getElementById("phone").value;
   if (!phone) {
     document.getElementById("phoneError").textContent = "電話番号は必須です。";
     errors.push("電話番号は必須です。");
   }
+
+  const dinnerCheckbox = document.getElementById("dinner").checked;
+  const breakfastCheckbox = document.getElementById("breakfast").checked;
+  const noMeal = document.querySelector("input[value='no_meal']");
+  if (
+    noMeal.checked === true &&
+    (dinnerCheckbox === true || breakfastCheckbox === true)
+  ) {
+    document.getElementById("nomealError").textContent = "素泊まりは食事の選択ができません。";
+    errors.push("素泊まりは食事の選択ができません。");
+  }
+
+  const withMeal = document.querySelector("input[value='with_meal']");
+  if (
+    withMeal.checked === true &&
+    dinnerCheckbox === false &&
+    breakfastCheckbox === false
+  ) {
+    document.getElementById("withmealError").textContent = "お食事を選択してください。";
+    errors.push("お食事を選択してください。");
+  }
+
   const checkin = document.getElementById("checkin").value;
   if (!checkin) {
     document.getElementById("checkinError").textContent =
@@ -187,29 +158,40 @@ reservationForm.addEventListener("submit", function (event) {
     errors.push("子どもの人数は必須です。");
   }
 
-  function toggleMealOptions() {
-    const plan = document.querySelector('input[name="plan"]:checked').value;
-    const dinnerCheckbox = document.getElementById("dinner");
-    const breakfastCheckbox = document.getElementById("breakfast");
-    if (plan === "with_meal") {
-      dinnerCheckbox.disabled = false;
-      breakfastCheckbox.disabled = false;
-    } else {
-      dinnerCheckbox.disabled = true;
-      breakfastCheckbox.disabled = true;
-      dinnerCheckbox.checked = false;
-      breakfastCheckbox.checked = false;
-    }
-  }
-  window.addEventListener("load", function () {
-    toggleMealOptions();
-  });
-
   const confirmationMessage = document.getElementById("confirmationMessage");
   if (errors.length === 0) {
     confirmationMessage.style.display = "block";
   }
+
+  console.log(errors);
 });
+
+const queryPlans = 'input[name="plan"]';
+const plans = document.querySelectorAll(queryPlans);
+plans.forEach((v) => {
+  v.addEventListener("click", toggleMealOptions);
+});
+
+function toggleMealOptions() {
+  const plan = document.querySelector('input[name="plan"]:checked');
+  if (!plan) return;
+
+  const mealType = plan.value;
+  const dinnerCheckbox = document.getElementById("dinner");
+  const breakfastCheckbox = document.getElementById("breakfast");
+
+  if (mealType === "with_meal") {
+    dinnerCheckbox.disabled = false;
+    breakfastCheckbox.disabled = false;
+    dinnerCheckbox.checked = true;
+    breakfastCheckbox.checked = true;
+  } else {
+    dinnerCheckbox.disabled = true;
+    breakfastCheckbox.disabled = true;
+    dinnerCheckbox.checked = false;
+    breakfastCheckbox.checked = false;
+  }
+}
 
 // 郵便番号での住所情報の取得
 function fetchAddressFromPostalCode() {
